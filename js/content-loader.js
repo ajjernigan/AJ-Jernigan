72e7726a70965ab622b6cdb6c605403cce4db9f4
kylin1209 <144865190+kylin1209@users.noreply.github.com>
2025-09-06 12:06:31 -0400

    use cloudinary to store images

--- js/content-loader.js
+++ js/content-loader.js
@@ -1,12 +1,12 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
 import { getFirestore, doc, getDoc } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js";
 
-// Firebase config
+// Firebase config - FIXED to use consistent storage bucket
 const firebaseConfig = {
   apiKey: "AIzaSyA3AuU1ZUK23LqewXhUPLeC_q3BG6mBN2I",
   authDomain: "aj-jernigan-site.firebaseapp.com",
   projectId: "aj-jernigan-site",
-  storageBucket: "aj-jernigan-site.firebasestorage.app",
+  storageBucket: "aj-jernigan-site.appspot.com", // CONSISTENT WITH OTHER FILES
   messagingSenderId: "823081137921",
   appId: "1:823081137921:web:8dc8a9d7fdddbf49048d00",
 };
